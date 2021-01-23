97ead430e44d462be32dada8d47c7348e22a3ed5
Youngwon Cho.develop <45826175+kingsman1960@users.noreply.github.com>
2021-01-24 00:25:20 +0900

    Update nypd_shooting_with_Streamlit.py

--- nypd_shooting_with_Streamlit.py
+++ nypd_shooting_with_Streamlit.py
@@ -76,6 +76,7 @@ hour = st.selectbox("Hour to look at", range(0, 24), 1)
 data = data[data['occur_date_occur_time'].dt.hour == hour]
 st.markdown("Shooting incident between %i:00 and %i:00" % (hour, (hour + 1) % 24))
 
+
 midpoint = (np.average(data["latitude"]), np.average(data["longitude"]))
 
 
